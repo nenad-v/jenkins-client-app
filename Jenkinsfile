f1d76601d243b5ab8552efff4b09764107e74d7d
Nenad <nvracar@ebay.com>
2020-02-13 14:34:08 +0100

    added Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        stage('Test') {
+            steps {
+                echo 'Running test'
+            }
+        }
+        stage('Build and Deploy') {
+            steps {
+                echo 'Running build and deploy'
+            }
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = 'http://localhost:5000/api';
+
+const App = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get(api + '/posts')
+            .then(res => {
+                setPosts(res.data)
+            })
+    }, [])
+
+    return (
+        <div className="app">
+            {
+                posts.map(post => (
+                    <div key={post.id}>
+                        <h4>{post.title}</h4>
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default App;
